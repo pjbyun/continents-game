29dc6349aab7a7b6a907d87fd3dcda9e020c5fba
Peter Byun <byunpeterj@gmail.com>
2020-11-13 03:18:34 -0500

    updated with code to play again

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const prompt = require('readline-sync');
 
 /* Global Variables */ 
 
-const continents = ['Africa', 'Antarctica', 'Asia', 'Australia', 'Europe', 'North America', 'South America'];
+let continents = ['Africa', 'Antarctica', 'Asia', 'Australia', 'Europe', 'North America', 'South America'];
 
 /* Dynamic Variables */
 let correctTotal = 7;
@@ -23,7 +23,7 @@ const contAns = (userAns) => {
                 userAns = prompt.question("Enter 'check' if you would like to see to what you've already answered. Otherwise type your next continent!\n\n")
             } else if (correctTotal === 0) {
                 console.log("🎊 Congrats, you win! 🎉 Wow, you really know your continents! 🗺");
-                break;
+                playAgain();
             }
         } else if (userAns == 'check') {
             console.log(userResp);
@@ -34,11 +34,27 @@ const contAns = (userAns) => {
             if (currentLives > 0) {
                 userAns = prompt.question("Enter 'check' if you would like to see to what you've already answered. Otherwise, type in your next answer!\n\n")
             } else if (currentLives === 0) {
-                console.log("You lose. Better start brushing up on your elementary geography... 😔")
+                console.log("You lose. Better start brushing up on your elementary geography... 😔");
+                playAgain();
             }
         }
     } 
 }
+
+const playAgain = () => {
+    let playAgain = prompt.question('Do you want to play again? y || n \n');
+    playAgain = playAgain.toLowerCase();
+    if (playAgain === 'y') {
+      continents = ['Africa', 'Antarctica', 'Asia', 'Australia', 'Europe', 'North America', 'South America'];
+      correctTotal = 7;
+      maxLives = 3;
+      userResp = [ ];
+      let newAns = prompt.question(`Let's see how well you know the continents of the world! If you answer incorrectly ${maxLives} times you will lose and have to start over. Type your answers in one at a time, writing the continent's full name. Spelling and punctuation counts. Ready, GO!.\n\n`)
+      contAns(newAns);
+    } else {
+      console.log('Thanks for playing.\nGoodbye.');
+    }
+  };
 
 contAns(userAns);
 
